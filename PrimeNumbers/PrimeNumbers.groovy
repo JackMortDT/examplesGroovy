442f086cc45c283fbefc65d9ac59d0d010094752
LuisSas <luis@makingdevs.com>
2017-12-12 10:12:54 -0600

    Sacando los factores primos con un numero que no sea primo

--- PrimeNumbers/PrimeNumbers.groovy
+++ PrimeNumbers/PrimeNumbers.groovy
@@ -1,5 +1,15 @@
 class PrimeNumbers{
-	int calculate(int number){
-        number
+    def calculate(int number){
+        int i = 0
+        int divisor = 2
+        def cadena = []
+        for(i = 0; i <= number; i++){
+            if(number % divisor == 0){
+                cadena.add(divisor)
+                number = number/divisor    
+            }
+            else divisor++
+        }
+        cadena.join(", ")
     }
 }
--- PrimeNumbers/PrimeNumbersTest.groovy
+++ PrimeNumbers/PrimeNumbersTest.groovy
@@ -3,16 +3,25 @@ class PrimeNumbersTests extends GroovyTestCase {
     PrimeNumbers primeNumbers = new PrimeNumbers()
     
     void testCalculateTwoOfPrimeFactors(){
-        int result = primeNumbers.calculate(2)
-        assert result == 2
+        def result = primeNumbers.calculate(2)
+        assert result == "2"
     }
     
     void testCalculateOtherSingleNumberOfPrimeFactors(){
-        int result = primeNumbers.calculate(3)
-        assert result == 3
+        def result = primeNumbers.calculate(3)
+        assert result == "3"
         result = primeNumbers.calculate(7)
-        result == 7
+        assert result == "7"
         result = primeNumbers.calculate(23)
-        result == 23
+        assert result == "23"
+    }
+
+    void testCalculateTwoNumbersOfPrimeFactors(){
+        def result = primeNumbers.calculate(4)
+        assert result == "2, 2"
+        result = primeNumbers.calculate(15)
+        assert result == "3, 5"
+        result = primeNumbers.calculate(141)
+        assert result == "3, 47"
     }
 }
