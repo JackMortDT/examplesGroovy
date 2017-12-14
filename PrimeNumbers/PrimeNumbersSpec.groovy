8b53ff206ef53b17681dbc8073bdf118846aa252
LuisSas <luis@makingdevs.com>
2017-12-14 00:02:34 -0600

    Cambiando test con GroovyTestCase a test con Spock

---
+++ PrimeNumbers/PrimeNumbersSpec.groovy
@@ -0,0 +1,32 @@
+@Grab(group='org.spockframework', module='spock-core', version='0.7-groovy-2.0')
+import spock.lang.*
+
+class PrimeNumbersSpec extends Specification {
+
+    def primeNumbers = new PrimeNumbers()
+    
+    void "Calculate the prime factors of a number"(){
+        given:"A number"
+            def number = row
+        when:
+            def result = primeNumbers.calculate(number)
+        then:
+            result == expect
+        where:
+            row     || expect
+            2       || "2"
+            3       || "3"
+            7       || "7"
+            23      || "23"
+            4       || "2, 2"
+            15      || "3, 5"
+            141     || "3, 47"
+            18      || "2, 3, 3"
+            27      || "3, 3, 3"
+            171     || "3, 3, 19"
+            140     || "2, 2, 5, 7"
+            180     || "2, 2, 3, 3, 5"
+            200     || "2, 2, 2, 5, 5"
+            1000000 || "2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5"
+    }
+}
