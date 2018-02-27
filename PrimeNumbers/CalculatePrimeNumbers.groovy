02797e6a74d07c04f52aa3b473ce52f10683bb2a
LuisSas <luis@makingdevs.com>
2018-02-27 00:16:02 -0600

    Pasando el programa a metodos

--- PrimeNumbers/CalculatePrimeNumbers.groovy
+++ PrimeNumbers/CalculatePrimeNumbers.groovy
@@ -1,25 +1,35 @@
 class CalculatePrimeNumbers{
-  String calculate(int numberReceived){
-    if(numberReceived == 1)
-      "1 no es primo"
+  String calculate(Integer numberReceived){
+    if(numberReceived == 1) "1 is not prime"
+    else if(numberReceived == 2) "2"
+    else calculateNumber(numberReceived)
+  }
 
-    else if(numberReceived == 2) 
-      "2"
+  String calculateNumber(Integer numberReceived){
+    List<Integer> listOfNumbers = []
+      iteratingNumber(numberReceived, listOfNumbers)
+  }
 
-    else{
-      List<Integer> listOfNumbers = []
-      (2..numberReceived).each{ iteratedNumber ->
-        Integer counter = 0
-        Integer currentNumber = 0
-        (1..iteratedNumber).each{ divider -> 
-          currentNumber = divider
-          ( iteratedNumber%divider == 0)? counter++:null
-          if (counter == 2) {
-            (currentNumber == iteratedNumber) ? listOfNumbers << iteratedNumber:null
-          }
-        }
+  String iteratingNumber(Integer numberReceived, List<Integer> listOfNumbers){
+    (2..numberReceived).each{ iteratedNumber ->
+        dividingNumber(iteratedNumber, listOfNumbers)
       }
       listOfNumbers.join(", ")
-    }     
-  }  
+  }
+
+  def dividingNumber(Integer iteratedNumber, List<Integer> listOfNumbers){
+    Integer counter = 0
+    Integer currentNumber = 0
+    (1..iteratedNumber).each{ divider -> 
+      currentNumber = divider
+      ( iteratedNumber%divider == 0)? counter++:null
+      assignNumberToList(counter, currentNumber, iteratedNumber, listOfNumbers)
+    }
+  }
+
+  List<Integer> assignNumberToList(Integer counter, Integer currentNumber, Integer iteratedNumber, List<Integer> listOfNumbers){
+    if (counter == 2) {
+      (currentNumber == iteratedNumber) ? listOfNumbers << iteratedNumber:null
+    }
+  }
 }
