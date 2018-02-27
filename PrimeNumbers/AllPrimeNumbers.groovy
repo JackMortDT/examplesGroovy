246240e5ddc006ffdf5e04aae07c4ac3448cdd38
LuisSas <luis@makingdevs.com>
2018-02-26 23:44:47 -0600

    Agregando programa que muestra todos los numeros primos hasta el numero pedido

---
+++ PrimeNumbers/AllPrimeNumbers.groovy
@@ -0,0 +1,23 @@
+Integer numberReceived = Integer.parseInt(System.console().readLine("Please enter your name: "))
+
+if(numberReceived == 1)
+  println("${numberReceived} No es primo")
+
+else if(numberReceived == 2) 
+  println("${numberReceived} Es primo")
+
+else{
+    List<Integer> listOfNumbers = []
+    (2..numberReceived).each{ iteratedNumber ->
+      Integer counter = 0
+      Integer currentNumber = 0
+        (1..iteratedNumber).each{ divider -> 
+          currentNumber = divider
+          ( iteratedNumber%divider == 0)? counter++:null
+          if (counter == 2) {
+            (currentNumber == iteratedNumber) ? listOfNumbers << iteratedNumber:null
+          }
+        }
+    }
+    println listOfNumbers.join(", ")
+}
