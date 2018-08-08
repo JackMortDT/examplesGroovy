b6fe31ae0d2ccd7c16542f035a747845fc883e00
LuisSas <luis@makingdevs.com>
2018-08-07 19:50:42 -0500

    Añadiendo prueba para el dia 3

---
+++ AdventOfCode/Day3/Day3Spec.groovy
@@ -0,0 +1,20 @@
+@Grab(group='org.spockframework', module='spock-core', version='1.1-groovy-2.4')
+import spock.lang.Specification
+
+class Day3Spec extends Specification{
+
+  def day3 = new Day3()
+
+  void "First Santa travel"(){
+    given:"The route"
+      String route = routes
+    when:"Santa's presents"
+      Integer totalTraveled = day3.routeForDeliverPresents(routes)
+    then:
+      assert totalTraveled == results
+    where:
+      routes || results
+      ">"    || 2
+      "^>v<" || 4
+  }
+}
